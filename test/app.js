13d77dbbb2591d28046aabf6fd11b91d96f91b11
Jérémie Ledentu <jledentu@gmail.com>
2016-10-11 10:00:31 +0200

    feat(styles): Unit tests

--- test/app.js
+++ test/app.js
@@ -1,21 +1,67 @@
-const path    = require('path');
+'use strict';
+
+const path = require('path');
 const helpers = require('yeoman-test');
-const assert  = require('yeoman-assert');
+const assert = require('yeoman-assert');
 
 /**
  * Test app generator.
  */
 describe('app', () => {
-  before((done) => {
-    // Mock the options, set up an output folder and run the generator
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .toPromise()
-      .then(() => {
-        done();
-      });
+
+  describe('default settings', () => {
+    before((done) => {
+      this.generator = helpers
+        .run(path.join(__dirname, '../generators/app'))
+        .toPromise()
+        .then(() => done());
+    });
+
+    it('should generate a README', () => {
+      assert.file('README.md');
+    });
+
+    it('should generate a main SASS file', () => {
+      assert.file('src/styles/main.scss');
+      assert.fileContent('src/styles/main.scss', '');
+    });
   });
 
-  it('should generate a README', () => {
-    assert.file('README.md');
+  describe('when using less', () => {
+
+    before((done) => {
+      this.generator = helpers
+        .run(path.join(__dirname, '../generators/app'))
+        .withPrompts({
+          cssPreprocessor: 'less'
+        })
+        .toPromise()
+        .then(() => done());
+    });
+
+    it('should generate a main LESS file', () => {
+      assert.file('src/styles/main.less');
+      assert.fileContent('src/styles/main.less', '');
+    });
+
+  });
+
+  describe('when using css', () => {
+
+    before((done) => {
+      this.generator = helpers
+        .run(path.join(__dirname, '../generators/app'))
+        .withPrompts({
+          cssPreprocessor: 'css'
+        })
+        .toPromise()
+        .then(() => done());
+    });
+
+    it('should generate a main CSS file', () => {
+      assert.file('src/styles/main.css');
+      assert.fileContent('src/styles/main.css', '');
+    });
+
   });
 });
