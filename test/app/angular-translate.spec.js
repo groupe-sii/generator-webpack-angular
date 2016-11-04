51443ef2ace8125d43e97572539e256d010ef0b1
Valentin GOT <gotvalentin@gmail.com>
2016-11-04 15:25:43 +0100

    test(angular-translate): add some tests

--- test/app/angular-translate.spec.js
+++ test/app/angular-translate.spec.js
@@ -19,12 +19,28 @@ module.exports = () => {
     });
 
     it('should add angular-translate to the package.json', () => {
-      assert.fileContent('package.json', 'angular-translate');
+      assert.fileContent('package.json', /"angular-translate"/);
+      assert.fileContent('package.json', /"angular-translate-loader-static-files"/);
     });
 
     it('should import angular-translate module', () => {
-      assert.fileContent('src/app/app.js', 'import \'angular-translate\'');
-      assert.fileContent('src/app/app.js', /angular\.module\([\s\S]*, [\s\S]*?restangular/);
+      assert.fileContent('src/app/app.js', 'import ngTranslate from \'angular-translate\'');
+      assert.fileContent('src/app/app.js', 'import ngTranslateLoaderStaticFiles from \'angular-translate-loader-static-files\'');
+      assert.fileContent('src/app/app.js', /angular\.module\([\s\S]*, [\s\S]*?ngTranslate/);
+      assert.fileContent('src/app/app.js', /angular\.module\([\s\S]*, [\s\S]*?ngTranslateLoaderStaticFiles/);
+    });
+
+    it('should copy locale files to build the build folder', () => {
+      assert.fileContent('webpack.config.js', '{ from: \'src/config/i18n\' }');
+    });
+
+    it('should generate default locale files', () => {
+      assert.file('src/config/i18n/fr.json');
+      assert.file('src/config/i18n/en.json');
+    });
+
+    it('should create an angular-translate section in config README', () => {
+      assert.fileContent('src/config/README.md', 'Angular Translate');
     });
 
   });
