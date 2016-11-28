1449ffa80e6d00a6581c1f2fc1ff1423ab1ffadd
Olivier LE LAY <olelay@RNSPC153.sii.fr>
2016-11-28 16:40:49 +0100

    valentin's quirk

--- templates/app/src/app/app.config.js
+++ templates/app/src/app/app.config.js
@@ -7,10 +7,8 @@ export default function (ENVIRONNEMENT, $locationProvider<% if (angularTranslate
   // Reference: https://docs.angularjs.org/api/ng/provider/$locationProvider#html5Mode
   $locationProvider.html5Mode(true);
 
-  if (ENVIRONNEMENT === 'prod') {
-    // Reference : http://blog.thoughtram.io/angularjs/2014/12/22/exploring-angular-1.3-disabling-debug-info.html
-    $compileProvider.debugInfoEnabled(false);
-  }
+  // Reference : http://blog.thoughtram.io/angularjs/2014/12/22/exploring-angular-1.3-disabling-debug-info.html
+  $compileProvider.debugInfoEnabled(ENVIRONNEMENT !== 'prod');
 
 <% if (angularTranslate) { -%>
   // Reference: https://angular-translate.github.io/docs/#/guide/12_asynchronous-loading#asynchronous-loading_using-staticfilesloader
