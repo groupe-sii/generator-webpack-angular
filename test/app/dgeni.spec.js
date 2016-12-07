8867efeb8657800047a711b36957cf2d819d662d
Olivier LE LAY <liollury@gmail.com>
2016-12-07 10:35:20 +0100

    test(docs): TU for docs

---
+++ test/app/dgeni.spec.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const path = require('path');
+const helpers = require('yeoman-test');
+const assert = require('yeoman-assert');
+
+module.exports = () => {
+
+  describe('when using docs', () => {
+
+    before(done => {
+      this.generator = helpers
+        .run(path.join(__dirname, '../../generators/app'))
+        .withPrompts({
+          appName: 'Webpack Generator'
+        })
+        .toPromise()
+        .then(() => done());
+    });
+
+    it('should add dgeni to the package.json', () => {
+      assert.fileContent('package.json', /"node \.\/tools\/docs-generator\/index\.js"/);
+      assert.fileContent('package.json', /"dgeni-packages"/);
+      assert.fileContent('package.json', /"dgeni"/);
+    });
+
+    it('should have default ngdocs files', () => {
+      assert.file('docs/api.ngdoc');
+      assert.file('docs/guide.ngdoc');
+      assert.file('docs/guide/howToUse.ngdoc');
+      assert.noFile('docs/_index.ngdoc');
+      assert.fileContent('docs/index.ngdoc', /Webpack Generator/);
+    });
+
+  });
+
+};
